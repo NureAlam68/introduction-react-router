f82f4c89ed80961d13b9865d2289a56354083dd3
NureAlam68 <nurealam151068@gmail.com>
2024-10-28 23:08:27 +0600

    nasted router

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,16 @@
+const Users = () => {
+    return (
+        <div>
+            <h2>Our top users:</h2>
+            <ul>
+                <li>Sohag</li>
+                <li>Saymon</li>
+                <li>Jui</li>
+                <li>Sazzad</li>
+                <li>Nihal</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Users;
